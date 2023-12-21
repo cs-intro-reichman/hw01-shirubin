98c1344186a632a266d44bf592a1c4c0b6917149
shirubin <154258705+shirubin@users.noreply.github.com>
2023-12-21 19:38:49 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,10 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	double a = Integer.parseInt(args[0]);
+        double b = Integer.parseInt(args[1]);
+        double c = Integer.parseInt(args[2]);
+        double solution = (c-b)/a;
+        System.out.println(a + " * x + " + b + " = " + c);
+        System.out.println("x = " + solution);
 }
