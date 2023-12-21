4ab68a889f5da22050e88f7287049c87b39c5f7e
shirubin <154258705+shirubin@users.noreply.github.com>
2023-12-21 19:36:09 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,9 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+        	int sum = num1 + num2;
+        	System.out.println(num1 + " + " + num2 + " = " + sum);
 	}
 }
