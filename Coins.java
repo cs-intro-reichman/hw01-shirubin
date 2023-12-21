322d2b657324235905e3225327aefa2fe854d0b4
shirubin <154258705+shirubin@users.noreply.github.com>
2023-12-21 19:43:54 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -7,6 +7,6 @@ public class Coins {
 		int numOfCents = Integer.parseInt(args[0]);
 	        int quarters = numOfCents/25;
 	        int centsLeft = numOfCents % 25;
-	        System.out.println("use " + quarters + " quarters and " + centsLeft + " cents");
+	        System.out.println("Use " + quarters + " quarters and " + centsLeft + " cents");
 	}
 }
